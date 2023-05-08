a9852cdd5a70b719cecaf1142a1e2b747c45cb91
sh237 <syun864297531@gmail.com>
2023-05-08 21:26:24 +0900

    [ADD] 5/8更新

--- split_val_unseen.py
+++ split_val_unseen.py
@@ -8,6 +8,16 @@ data_dir = os.path.join(os.environ['DATA'], 'generated_2.1.0')
 with open(os.path.join(data_dir, 'processed_original.txt'), 'r') as f:
     lines = f.readlines()
 
+#./valid_unseen以下のディレクトリ一覧を取得()
+dirs = glob.glob(os.path.join(data_dir, 'valid_unseen/*/*'))
+
+# task_dirs = glob.glob(os.path.join(data_dir, ' valid_unseen/*'))
+
+data_path = os.path.join(data_dir, 'valid_unseen')
+task_dirs = [name for name in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, name))]
+
+print("task_dirs", task_dirs)
+
 result = []
 pseudo_valid_list = []
 pseudo_test_list = []
@@ -15,13 +25,25 @@ pseudo_test_list = []
 result_pseudo_valid = []
 result_pseudo_test = []
 result_others = []
+
+valid_tasks = []
+test_tasks = []
+for index, task_dir in enumerate(task_dirs):
+    if index % 2 == 0:
+        valid_tasks.append(task_dir)
+    else:
+        test_tasks.append(task_dir)
+
+print(task_dirs)
+# print(test_tasks)
+
 for index, line in enumerate(lines):
     if index < 255:
-        if index % 2 == 0:
+        if line.split('/')[1] in valid_tasks:
             pseudo_valid_list.append(line.split('/')[1] + '/' + line.split('/')[2])
             #各lineの先頭のvalid_unseenをpseudo_validに変更
             result_pseudo_valid.append(line.replace('valid_unseen', 'pseudo_valid',1))
-        else:
+        elif line.split('/')[1] in test_tasks:
             pseudo_test_list.append(line.split('/')[1] + '/' + line.split('/')[2])
             #各lineの先頭のvalid_unseenをpseudo_testに変更
             result_pseudo_test.append(line.replace('valid_unseen', 'pseudo_test',1))
@@ -29,9 +51,6 @@ for index, line in enumerate(lines):
         result_others.append(line)
 
 result = result_pseudo_valid + result_pseudo_test + result_others
-
-#./valid_unseen以下のディレクトリ一覧を取得()
-dirs = glob.glob(os.path.join(data_dir, 'valid_unseen/*/*'))
 
 # print("result:", result)
 # print("dirs:", dirs)
@@ -45,16 +64,13 @@ os.mkdir(os.path.join(data_dir, 'pseudo_test'))
 
 print("len(dirs)", len(dirs))
 
-a = []
 for d in dirs:
     if d.split('/')[-2] + '/' + d.split('/')[-1] in pseudo_valid_list:
         if os.path.exists(os.path.join(data_dir, 'pseudo_valid', d.split('/')[-2])):
             shutil.copytree(d, os.path.join(data_dir, 'pseudo_valid', d.split('/')[-2], d.split('/')[-1]))
-            a.append(os.path.join(data_dir, 'pseudo_valid', d.split('/')[-2], d.split('/')[-1]))
         else:
             os.mkdir(os.path.join(data_dir, 'pseudo_valid', d.split('/')[-2]))
             shutil.copytree(d, os.path.join(data_dir, 'pseudo_valid', d.split('/')[-2], d.split('/')[-1]))
-            a.append(os.path.join(data_dir, 'pseudo_valid', d.split('/')[-2], d.split('/')[-1]))
     elif d.split('/')[-2] + '/' +  d.split('/')[-1] in pseudo_test_list:
         if os.path.exists(os.path.join(data_dir, 'pseudo_test', d.split('/')[-2])):
             shutil.copytree(d, os.path.join(data_dir, 'pseudo_test', d.split('/')[-2], d.split('/')[-1]))
@@ -62,7 +78,6 @@ for d in dirs:
             os.mkdir(os.path.join(data_dir, 'pseudo_test', d.split('/')[-2]))
             shutil.copytree(d, os.path.join(data_dir, 'pseudo_test' , d.split('/')[-2], d.split('/')[-1]))
 
-print("len(a):",len(a))
 
 #processed_new.txtに書き込む
 with open(os.path.join(data_dir, 'processed_temp.txt'), 'w') as f:

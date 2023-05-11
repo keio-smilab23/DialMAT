a5043613ee85280e262db2a51daf6524662e1146
sh237 <syun864297531@gmail.com>
2023-05-11 17:22:11 +0900

    [CHANGE] pseudo_testを推論対象に変更

--- train_eval.py
+++ train_eval.py
@@ -665,7 +665,7 @@ def trainModel(args):
 def evalModel(args):
     np.random.seed(0)
     # data_split = "unseen"
-    data_split = "pseudo_valid"
+    data_split = "pseudo_test"
     # data_split = "valid_unseen"
     train_id = 1
     logging.basicConfig(filename='./logs/rl_anytime_eval_'+ data_split + str(train_id) + '.log', level=logging.INFO)
